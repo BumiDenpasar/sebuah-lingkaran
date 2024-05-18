19586f7ae0530a2a54776a790c18c8552fb228d2
BumiDenpasar <komangtagnan33@gmail.com>
2024-05-18 14:03:00 +0700

    fix errors

--- app/error/[message]/page.tsx
+++ app/error/[message]/page.tsx
@@ -19,7 +19,7 @@ export default function Page({params}:any) {
                 <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                     <div className="mx-auto max-w-screen-sm text-center flex flex-col items-center">
                         <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">Error</h1>
-                        <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Something's missing.</p>
+                        <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Somethings missing.</p>
                         <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">{decodeURIComponent(message)}</p>
                         <Link
                             href="/"
