5b55b47794416af281b34433b23abf179da154fd
BumiDenpasar <komangtagnan33@gmail.com>
2024-05-18 15:51:31 +0700

    fix ui error

--- components/GroupId.tsx
+++ components/GroupId.tsx
@@ -5,7 +5,7 @@ export default function GroupId(props: any) {
   const router = useRouter();
 
   if (!props.valid) {
-    router.push('error/Sirkel yang kamu cari gak ada :(');
+    router.push('error/Sirkel yang kamu cari gak ada');
     return null;
   }
 }
